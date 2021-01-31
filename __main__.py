f65d59d03092ec745b930d3baffabe71bf9e16ef
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-01-31 02:25:12 -0700

    beautified

--- __main__.py
+++ __main__.py
@@ -7,6 +7,8 @@ import os
 app = Flask(__name__)
 
 now = datetime.now()
+
+
 @app.route('/')
 def my_form():
     return render_template('main.html')
@@ -35,14 +37,19 @@ def index():
 def my_form_shutdown():
     os.system('shutdown /t 300 /s')
     return redirect('/')
+
+
 @app.route('/logout')
 def my_form_logout():
     os.system('shutdown /l')
     return redirect('/')
+
+
 @app.route('/cshut')
 def my_form_cshut():
     os.system('shutdown /a')
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=6969)
